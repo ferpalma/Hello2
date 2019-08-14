52fca9190ebb843660471e9b032ff14c3dc032b6
pat001714 <pat001714@172.16.91.117>
2019-08-14 09:13:26 -0300

    Criando projeto

---
+++ src/br/unitins/hello/controller/HelloController.java
@@ -0,0 +1,11 @@
+package br.unitins.hello.controller;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+@Named
+@RequestScoped
+public class HelloController{
+
+
+}
---
+++ src/br/unitins/hello/controller/LoginController.java
@@ -0,0 +1,34 @@
+package br.unitins.hello.controller;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+@Named
+@RequestScoped
+public class LoginController {
+
+	private String login = "João";
+	private String senha = "123";
+	
+	public void logar() {
+		System.out.println(login);
+		System.out.println(senha);
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+}
